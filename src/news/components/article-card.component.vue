3daa9c121d760b3dade772744391610edbc84519
Adrian Melgar <DaikiMT@outlook.es>
2023-09-04 20:56:07 -0500

    feat(news): added particle card

---
+++ src/news/components/article-card.component.vue
@@ -0,0 +1,45 @@
+<template>
+  <pv-card class="m-2">
+    <template #header>
+      <img :src="article.utlToImage"
+           :alt="article.title">
+    </template>
+    <template #title>
+      <p>{{ article.title }}</p>
+    </template>
+    <template #content>
+      <p class="flex align-content-start flex-wrap">
+        <span class="flex align-items-center justify-content-center">
+          <pv-avatar :image="article.source.urlToLogo"
+                     :aria-label="article.source.name"
+                     shape="circle"/>
+        </span>
+        <span class="flex align-items-center justify-content-center">
+          {{article.source.name}}
+        </span>
+      </p>
+      <p class="flex align-content-start flex-wrap mt-4">
+        {{ article.description }}
+      </p>
+    </template>
+    <template #footer>
+
+    </template>
+  </pv-card>
+</template>
+
+<script setup>
+
+export default {
+  name: "article-card",
+  props: {
+    article: null,
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
